07903f3f8e1b9a389a26488b0f23c8c72932afba
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-29 20:09:57 +0100

    search component adjustments

--- components/Search.tsx
+++ components/Search.tsx
@@ -4,9 +4,9 @@ import {Flex, Input} from '@chakra-ui/react';
 
 export default function Search() {
   return (
-    <Flex width="25%" boxShadow="base" alignItems="center" mt="10" p="4" justifyContent="space-evenly" borderRadius="md">
+    <Flex width="25%" boxShadow="base" alignItems="center" p="4" justifyContent="space-evenly" borderRadius="md">
         <SearchIcon color="gray.500" ml={3} mr={5}/>
-        <Input variant="unstyled" placeholder="Search for a country..." fontSize="xs"/>
+        <Input variant="unstyled" placeholder="Search for a country..." fontSize="sm"/>
         <ArrowForwardIcon color="gray.500"/>
     </Flex>
   )
