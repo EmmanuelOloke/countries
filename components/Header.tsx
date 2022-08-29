0ab529e9a6f832dbde638a96ec695383214653a6
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-29 17:49:45 +0100

    header component adjustments

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import {Flex, Text, IconButton, useColorMode} from '@chakra-ui/react'
-import {MoonIcon, SunIcon} from '@chakra-ui/icons'
+import React from 'react';
+import {Flex, Text, IconButton, useColorMode} from '@chakra-ui/react';
+import {MoonIcon, SunIcon} from '@chakra-ui/icons';
 
 export default function Header() {
     const {colorMode, toggleColorMode} = useColorMode();
   return (
-    <Flex p={6} justifyContent="space-between" boxShadow='md'>
-        <Text fontSize="x-large" fontWeight="bold">Where in the world?</Text>
-        <Flex alignItems="center">
-            <IconButton area-label="icon" icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />} onClick={toggleColorMode}/>
-            <Text marginLeft="3">Dark Mode</Text>
+    <Flex boxShadow='md' justifyContent='center'>
+        <Flex p={6} justifyContent="space-between" width="90%">
+            <Text fontSize="x-large" fontWeight="bold">Where in the world?</Text>
+            <Flex alignItems="center">
+                <IconButton aria-label="icon" icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />} onClick={toggleColorMode}/>
+                <Text marginLeft="3">Dark Mode</Text>
+            </Flex>
         </Flex>
     </Flex>
   )
