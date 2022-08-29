a791fabf4b0d3fbb1a2b4a6c4d9e7fd742ea07ac
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-29 14:56:03 +0100

    IconButton, useColorMode, toggleColorMode

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import {Flex, Text} from '@chakra-ui/react'
+import {Flex, Text, IconButton, useColorMode} from '@chakra-ui/react'
+import {MoonIcon, SunIcon} from '@chakra-ui/icons'
 
 export default function Header() {
+    const {colorMode, toggleColorMode} = useColorMode();
   return (
     <Flex p={6} justifyContent="space-between" boxShadow='md'>
         <Text fontSize="x-large" fontWeight="bold">Where in the world?</Text>
-        <Flex>
-            <Text>Dark Mode</Text>
+        <Flex alignItems="center">
+            <IconButton area-label="icon" icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />} onClick={toggleColorMode}/>
+            <Text marginLeft="3">Dark Mode</Text>
         </Flex>
     </Flex>
   )
