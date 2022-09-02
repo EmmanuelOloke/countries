6e6dd3120e0d045c6d2dae95e3ffc85cb7cb4a4e
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-02 23:37:32 +0100

    responsive width

--- components/SearchAndFilter.tsx
+++ components/SearchAndFilter.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import {Flex} from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import Filter from '../components/Filter';
 import Search from '../components/Search';
 
 export default function SearchAndFilter() {
   return (
     <Flex mt="50" justifyContent="center">
-        <Flex alignItems="center" justifyContent="space-between" width="87%">
-            <Search/>
-            <Filter/>
-        </Flex>
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        w="87%"
+        maxW={{ base: '87vw', sm: '80vw', lg: '87vw', xl: '87vw' }}
+      >
+        <Search />
+        <Filter />
+      </Flex>
     </Flex>
-  )
+  );
 }
