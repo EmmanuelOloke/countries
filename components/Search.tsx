cc7ed0506efe6e70f0fb70db798b02e8922c83bf
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-04 21:55:28 +0100

    search refactor...

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
-import {SearchIcon, ArrowForwardIcon} from '@chakra-ui/icons';
-import {Flex, Input} from '@chakra-ui/react';
+import React, { MouseEvent, useState } from 'react';
+import { SearchIcon, ArrowForwardIcon } from '@chakra-ui/icons';
+import { Button, HStack, Input } from '@chakra-ui/react';
 
 export default function Search() {
+  const [inputContent, setInputContent] = useState('');
+
+  const handleSubmit = async (e: MouseEvent) => {
+    e.preventDefault();
+
+    // interface Country {
+    //   name?: object;
+    //   common?: string;
+    // }
+
+    if (!inputContent) {
+      console.log('enter something abeg');
+    }
+
+    const response = await fetch(`https://restcountries.com/v3.1/name/${inputContent}`);
+    const countries = await response.json();
+    console.log(countries[0].name.common);
+  };
+
   return (
-    <Flex width="25%" boxShadow="base" alignItems="center" p="4" justifyContent="space-evenly" borderRadius="md">
-        <SearchIcon color="gray.500" ml={3} mr={5}/>
-        <Input variant="unstyled" placeholder="Search for a country..." fontSize="sm"/>
-        <ArrowForwardIcon color="gray.500"/>
-    </Flex>
-  )
+    <HStack
+      width="25%"
+      boxShadow="base"
+      alignItems="center"
+      p="4"
+      justifyContent="space-evenly"
+      borderRadius="md"
+    >
+      <SearchIcon color="gray.500" ml={3} mr={2} />
+      <Input
+        variant="unstyled"
+        placeholder="Search for a country..."
+        fontSize="sm"
+        value={inputContent}
+        onChange={(e) => setInputContent(e.target.value)}
+      />
+      <Button onClick={(e) => handleSubmit(e)}>
+        <ArrowForwardIcon color="gray.500" cursor="pointer" />
+      </Button>
+    </HStack>
+  );
 }
