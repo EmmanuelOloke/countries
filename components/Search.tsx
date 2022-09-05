456db754c87145332209c0b0c8575804c0ed7736
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-05 18:08:57 +0100

    wrapped search request in try, catch

--- components/Search.tsx
+++ components/Search.tsx
@@ -25,20 +25,24 @@ const Search: React.FC<Props> = ({ countries, setCountries }) => {
       return;
     }
 
-    const response = await fetch(`https://restcountries.com/v3.1/name/${inputContent}`);
-    const searchCountries = await response.json();
+    try {
+      const response = await fetch(`https://restcountries.com/v3.1/name/${inputContent}`);
+      const searchCountries = await response.json();
 
-    if (response.status !== 200) {
-      toast({
-        title: 'Country Not Found',
-        status: 'error',
-        duration: 5000,
-        isClosable: true,
-        position: 'top',
-      });
-      return;
-    } else {
-      setCountries(searchCountries);
+      if (response.status !== 200) {
+        toast({
+          title: 'Country Not Found',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+          position: 'top',
+        });
+        return;
+      } else {
+        setCountries(searchCountries);
+      }
+    } catch (error) {
+      console.log('Something went wrong', error);
     }
   };
 
