cf856fa0ba19a9dd318a53acf746d13fb7c81b1c
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-06 14:09:22 +0100

    received countries and setCountries props, make API call to update the setCountries state

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import {Box} from '@chakra-ui/react';
-import {Select} from "chakra-react-select";
+import React, { Dispatch, SetStateAction } from 'react';
+import { Box } from '@chakra-ui/react';
+import { Select, SingleValue } from 'chakra-react-select';
 
-export default function Filter() {
-    const options = [
-        { value: 'africa', label: 'Africa' },
-        { value: 'america', label: 'America' },
-        { value: 'asia', label: 'Asia' },
-        { value: 'europe', label: 'Europe' },
-        { value: 'oceania', label: 'Oceania' }
-      ]
-
-    return (
-        <Box w="250px">
-            <Select size="lg" placeholder="Filter by Region" focusBorderColor='none' options={options}/>
-        </Box>
-    )
+interface Props {
+  countries: any[];
+  setCountries: Dispatch<SetStateAction<any[]>>;
 }
+
+const Filter: React.FC<Props> = ({ countries, setCountries }) => {
+  const options = [
+    { value: 'africa', label: 'Africa' },
+    { value: 'america', label: 'America' },
+    { value: 'asia', label: 'Asia' },
+    { value: 'europe', label: 'Europe' },
+    { value: 'oceania', label: 'Oceania' },
+  ];
+
+  const handleSelectChange = async (data: SingleValue<{ value: string; label: string }>) => {
+    try {
+      const response = await fetch(`https://restcountries.com/v3.1/region/${data?.value}`);
+      const filteredCountries = await response.json();
+
+      setCountries(filteredCountries);
+    } catch (error) {
+      console.log('Something went wrong', error);
+    }
+  };
+
+  return (
+    <Box w="250px">
+      <Select
+        size="lg"
+        placeholder="Filter by Region"
+        focusBorderColor="none"
+        options={options}
+        onChange={(data) => handleSelectChange(data)}
+      />
+    </Box>
+  );
+};
+
+export default Filter;
