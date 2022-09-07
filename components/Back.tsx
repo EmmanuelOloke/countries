6dcabb740155cef26275b7aa6b0690e4eadc23b9
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-08 00:24:19 +0100

    back button component design

--- components/Back.tsx
+++ components/Back.tsx
@@ -1,7 +1,17 @@
+import { Button, Flex } from '@chakra-ui/react';
 import React from 'react';
+import { ArrowBackIcon } from '@chakra-ui/icons';
 
 const Back = () => {
-  return <div>Back</div>;
+  return (
+    <Flex justifyContent="center">
+      <Flex mt={50} w="87%">
+        <Button leftIcon={<ArrowBackIcon />} variant="outline" pl={5} pr={10} boxShadow="md">
+          Back
+        </Button>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Back;
