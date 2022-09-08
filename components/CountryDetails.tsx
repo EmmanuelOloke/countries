a5fe23412f41ce061557fef1f35679dae4303abc
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-08 15:47:31 +0100

    object manipulation for languages and currencies

--- components/CountryDetails.tsx
+++ components/CountryDetails.tsx
@@ -7,6 +7,18 @@ interface Props {
 }
 
 const CountryDetails: React.FC<Props> = ({ countryData }) => {
+  const getLanguages = () => {
+    const languages = Object.entries(countryData.languages);
+    const list = languages.map((language) => {
+      return ` ${language[1]}`;
+    });
+    return list.toString();
+  };
+
+  const getValue = () => {
+    const values: any = Object.values(countryData.currencies);
+    return values[0].name;
+  };
   return (
     <Flex mt="3em" justifyContent="center" alignItems="center">
       <Flex w="87%" flexDirection="row" gap={100}>
@@ -21,41 +33,43 @@ const CountryDetails: React.FC<Props> = ({ countryData }) => {
         <Flex flexDirection="column">
           <Heading>{countryData.name.common}</Heading>
 
-          <Flex flexDirection="column">
-            <Flex flexDirection="row" mt={30} mb={2}>
-              <Text as="b">Native Name</Text>
-              <Text>: {countryData.name.common}</Text>
+          <Flex flexDirection="row" gap={150}>
+            <Flex flexDirection="column">
+              <Flex flexDirection="row" mt={30} mb={2}>
+                <Text as="b">Native Name</Text>
+                <Text>: {countryData.name.common}</Text>
+              </Flex>
+              <Flex flexDirection="row" mb={2}>
+                <Text as="b">Population</Text>
+                <Text>: {countryData.population}</Text>
+              </Flex>
+              <Flex flexDirection="row" mb={2}>
+                <Text as="b">Region</Text>
+                <Text>: {countryData.region}</Text>
+              </Flex>
+              <Flex flexDirection="row" mb={2}>
+                <Text as="b">Sub Region</Text>
+                <Text>: {countryData.subregion}</Text>
+              </Flex>
+              <Flex flexDirection="row" mb={2}>
+                <Text as="b">Capital</Text>
+                <Text>: {countryData.capital}</Text>
+              </Flex>
             </Flex>
-            <Flex flexDirection="row" mb={2}>
-              <Text as="b">Population</Text>
-              <Text>: {countryData.population}</Text>
-            </Flex>
-            <Flex flexDirection="row" mb={2}>
-              <Text as="b">Region</Text>
-              <Text>: {countryData.region}</Text>
-            </Flex>
-            <Flex flexDirection="row" mb={2}>
-              <Text as="b">Sub Region</Text>
-              <Text>: {countryData.subregion}</Text>
-            </Flex>
-            <Flex flexDirection="row" mb={2}>
-              <Text as="b">Capital</Text>
-              <Text>: {countryData.capital}</Text>
-            </Flex>
-          </Flex>
 
-          <Flex>
-            <Flex flexDirection="row" mt={30} mb={2}>
-              <Text as="b">Native Name</Text>
-              <Text>: {countryData.name.common}</Text>
-            </Flex>
-            <Flex flexDirection="row" mb={2}>
-              <Text as="b">Population</Text>
-              <Text>: {countryData.population}</Text>
-            </Flex>
-            <Flex flexDirection="row" mb={2}>
-              <Text as="b">Region</Text>
-              <Text>: {countryData.region}</Text>
+            <Flex flexDirection="column">
+              <Flex flexDirection="row" mt={30} mb={2}>
+                <Text as="b">Top Level Domain</Text>
+                <Text>: {countryData.tld}</Text>
+              </Flex>
+              <Flex flexDirection="row" mb={2}>
+                <Text as="b">Currencies</Text>
+                <Text>: {getValue()}</Text>
+              </Flex>
+              <Flex flexDirection="row" mb={2}>
+                <Text as="b">Languages</Text>
+                <Text>: {getLanguages()}</Text>
+              </Flex>
             </Flex>
           </Flex>
         </Flex>
