f9c328e2581f1b1b31057d0341556b269cdd9e6c
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-08 03:34:22 +0100

    details page design progress

--- components/CountryDetails.tsx
+++ components/CountryDetails.tsx
@@ -1,3 +1,5 @@
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import Image from 'next/image';
 import React from 'react';
 
 interface Props {
@@ -5,7 +7,61 @@ interface Props {
 }
 
 const CountryDetails: React.FC<Props> = ({ countryData }) => {
-  return <h1>{countryData?.name?.common}</h1>;
+  return (
+    <Flex mt="3em" justifyContent="center" alignItems="center">
+      <Flex w="87%" flexDirection="row" gap={100}>
+        <Image
+          height={350}
+          width={470}
+          layout="fixed"
+          src={countryData.flags.svg}
+          alt="country flag"
+        />
+
+        <Flex flexDirection="column">
+          <Heading>{countryData.name.common}</Heading>
+
+          <Flex flexDirection="column">
+            <Flex flexDirection="row" mt={30} mb={2}>
+              <Text as="b">Native Name</Text>
+              <Text>: {countryData.name.common}</Text>
+            </Flex>
+            <Flex flexDirection="row" mb={2}>
+              <Text as="b">Population</Text>
+              <Text>: {countryData.population}</Text>
+            </Flex>
+            <Flex flexDirection="row" mb={2}>
+              <Text as="b">Region</Text>
+              <Text>: {countryData.region}</Text>
+            </Flex>
+            <Flex flexDirection="row" mb={2}>
+              <Text as="b">Sub Region</Text>
+              <Text>: {countryData.subregion}</Text>
+            </Flex>
+            <Flex flexDirection="row" mb={2}>
+              <Text as="b">Capital</Text>
+              <Text>: {countryData.capital}</Text>
+            </Flex>
+          </Flex>
+
+          <Flex>
+            <Flex flexDirection="row" mt={30} mb={2}>
+              <Text as="b">Native Name</Text>
+              <Text>: {countryData.name.common}</Text>
+            </Flex>
+            <Flex flexDirection="row" mb={2}>
+              <Text as="b">Population</Text>
+              <Text>: {countryData.population}</Text>
+            </Flex>
+            <Flex flexDirection="row" mb={2}>
+              <Text as="b">Region</Text>
+              <Text>: {countryData.region}</Text>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default CountryDetails;
