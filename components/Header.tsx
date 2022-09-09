6159c541a7063774fa36d6038d2a137e5b93db6b
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-09 21:22:59 +0100

    max width adjusstment

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import {Flex, Text, IconButton, useColorMode} from '@chakra-ui/react';
-import {MoonIcon, SunIcon} from '@chakra-ui/icons';
+import { Flex, Text, IconButton, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 export default function Header() {
-    const {colorMode, toggleColorMode} = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Flex boxShadow='md' justifyContent='center'>
-        <Flex p={6} justifyContent="space-between" width="90%">
-            <Text fontSize="x-large" fontWeight="bold" style={{fontFamily: "'Nunito Sans', sans-serif"}}>Where in the world?</Text>
-            <Flex alignItems="center">
-                <IconButton aria-label="icon" icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />} onClick={toggleColorMode}/>
-                <Text marginLeft="3">Dark Mode</Text>
-            </Flex>
+    <Flex boxShadow="md" justifyContent="center">
+      <Flex
+        p={5}
+        justifyContent="space-between"
+        width="90%"
+        maxW={{ base: '87vw', sm: '80vw', md: '80vw', lg: '88vw', xl: '88vw', '2xl': '67vw' }}
+      >
+        <Text
+          fontSize="x-large"
+          fontWeight="bold"
+          style={{ fontFamily: "'Nunito Sans', sans-serif" }}
+        >
+          Where in the world?
+        </Text>
+        <Flex alignItems="center">
+          <IconButton
+            aria-label="icon"
+            icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            onClick={toggleColorMode}
+          />
+          <Text marginLeft="2">Dark Mode</Text>
         </Flex>
+      </Flex>
     </Flex>
-  )
+  );
 }
