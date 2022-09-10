bda36158a516a53e068eb226edc2b14711919c06
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-10 01:12:26 +0100

    SearchAndFilter responsiveness

--- components/SearchAndFilter.tsx
+++ components/SearchAndFilter.tsx
@@ -12,9 +12,25 @@ const SearchAndFilter: React.FC<Props> = ({ countries, setCountries }) => {
   return (
     <Flex mt="50" justifyContent="center">
       <Flex
-        alignItems="center"
-        justifyContent="space-between"
+        alignItems={{
+          base: 'left',
+          sm: 'left',
+          md: 'left',
+          lg: 'center',
+          xl: 'center',
+          '2xl': 'center',
+        }}
+        justifyContent={{
+          base: 'flex-start',
+          sm: 'flex-start',
+          md: 'flex-start',
+          lg: 'space-between',
+          xl: 'space-between',
+          '2xl': 'space-between',
+        }}
+        flexDirection={{ base: 'column', lg: 'row', xl: 'row', '2xl': 'row' }}
         w="87%"
+        gap={{ base: '50' }}
         maxW={{ base: '87vw', sm: '80vw', md: '80vw', lg: '87vw', xl: '87vw', '2xl': '65vw' }}
       >
         <Search countries={countries} setCountries={setCountries} />
