a03b1385c9bab41117a80fff1372fa602ba901ef
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-12 23:39:48 +0100

    width adjustments

--- components/Back.tsx
+++ components/Back.tsx
@@ -5,7 +5,10 @@ import { ArrowBackIcon } from '@chakra-ui/icons';
 const Back = () => {
   return (
     <Flex justifyContent="center">
-      <Flex mt={58} w="87%">
+      <Flex
+        mt={58}
+        w={{ base: '87vw', sm: '80vw', md: '80vw', lg: '88vw', xl: '87vw', '2xl': '65vw' }}
+      >
         <Button
           leftIcon={<ArrowBackIcon />}
           variant="outline"
