b937b16b32031a3c0de83244ad721d558fefa391
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-09-16 01:25:21 +0100

    globally added loading spinner styles to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import '../components/LoadingSpinner/LoadingSpinner.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
